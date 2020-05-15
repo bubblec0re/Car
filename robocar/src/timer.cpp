4e936ea13b47c9427d77834e29a484ea67bf4ae3
Vladimir <vova206@gmail.com>
2020-05-15 22:40:03 +0300

    Started total rewriting. Added timer class.

---
+++ robocar/src/timer.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+class Timer
+{
+public:
+    Timer(uint64_t periodms)
+    {
+        _period = periodms;
+        _checkTime = millis();
+    }
+
+    bool itsTime()
+    {
+        uint64_t now = millis();
+        if (now - _checkTime > _period)
+        {
+            _checkTime = millis();
+            return true;
+        }
+        return false;
+    }
+
+private:
+    uint64_t _period, _checkTime;
+};
